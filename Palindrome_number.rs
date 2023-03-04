c427d603a2524a4c1130ae5cedb85c26173019b6
Tony <Tony@Tony.com>
2023-03-04 13:31:35 +0330

    main Code

---
+++ Palindrome_number.rs
@@ -0,0 +1,40 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        
+        if x < 0 {
+            return false
+        }
+        else if x == 0 {
+            return true
+        }
+        else{
+            let mut rev_of_num = Vec::with_capacity(x.clone().to_string().chars().count());
+
+
+            for (_,item) in x.to_string().chars().enumerate(){
+                rev_of_num.push(item);
+            };
+
+            let len = x.clone().to_string().chars().count();
+
+            if rev_of_num[len-1] == '0' {
+                return false
+            }else{
+                rev_of_num.reverse();
+
+                let mut rev_str = String::new();
+
+                for i in rev_of_num{
+                    rev_str.push(i)
+                };
+
+                if rev_str == x.to_string() {
+                    return true
+                }else{
+                    return false
+                };
+            }
+
+        }
+    }   
+}
